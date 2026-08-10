8dfeb00e4aea86e284e9ec43397c54b8716ea011
Badhan Sarkar <badhansarkar164@gmail.com>
2026-08-10 21:09:41 +0530

    add config

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,12 +1,19 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 	"time"
+
+	"github.com/programmer-bell/go-monolith/internal/config"
 )
 
 func main() {
+
+	cfg := config.MustLoad()
+
+	fmt.Println("Starting olx server")
 
 	mux := http.NewServeMux()
 
@@ -17,13 +24,14 @@ func main() {
 	})
 
 	srv := http.Server{
-		Addr:         ":8000",
+		Addr:         ":" + cfg.Port,
 		Handler:      mux,
 		ReadTimeout:  time.Second * 10,
 		WriteTimeout: time.Second * 30,
 		IdleTimeout:  time.Second * 60,
 	}
 
+	log.Printf("Server is listenning %s", srv.Addr)
 	if err := srv.ListenAndServe(); err != nil {
 		log.Fatalf("Failed to start server: %v", err)
 	}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/programmer-bell/go-monolith
 
 go 1.26.5
+
+require github.com/joho/godotenv v1.5.1
